c88f8c855364ef6b9c8efcf22f21a7cd1c390519
Iván Mera <ivan@cajanegra.com.ec>
2016-09-08 06:13:45 -0500

    Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	 teacher = Teacher.find_by(email: params[:session][:email].downcase)
+  	 if teacher && teacher.authenticate(params[:session][:password])
+  	 	log_in teacher
+  	 	redirect_to teacher
+  	 else
+  	 	flash.now[:danger] = 'Invalid email/password combination'
+  	 	render 'new'
+  	 end
+  end
+
+  def destroy
+  end
+end
