20f30b5ab496d083338c982be7dda74131f28dda
Iván Mera <ivan@cajanegra.com.ec>
2016-09-08 00:35:31 -0500

    Correccion discrepancia en formulario enviado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/signup',    to: 'teachers#new'
-  post '/signup',  to: 'teacher#create'
+  post '/signup',  to: 'teachers#create'
   resources :teachers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
